9acd2208e839dfb4f69e977430e6da9d9a610770
potetakoyaki <potetakoyaki@gmail.com>
2021-03-17 01:19:12 +0000

    [upload]top

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
-Rails.application.routes.draw do
+ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  #homes
+  root 'homes#top'
+  get 'homes/about' => "homes#about"
+ 
+  #devise
+  devise_for :publics
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
 end
