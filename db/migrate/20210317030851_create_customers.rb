7039a5cd0b678551d7f38c0cef92a037912d37f3
kayamayoki <k32142004@gmail.com>
2021-03-17 05:20:09 +0000

    カラム追加

--- db/migrate/20210317030851_create_customers.rb
+++ db/migrate/20210317030851_create_customers.rb
@@ -2,6 +2,16 @@ class CreateCustomers < ActiveRecord::Migration[5.2]
   def change
     create_table :customers do |t|
 
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :email
+      t.string :postal_code
+      t.string :telephone_number
+      t.string :address
+      t.boolean :is_deleted, default: false
+
       t.timestamps
     end
   end
--- db/migrate/20210317031025_create_oreder_details.rb
+++ db/migrate/20210317031025_create_oreder_details.rb
@@ -2,6 +2,12 @@ class CreateOrederDetails < ActiveRecord::Migration[5.2]
   def change
     create_table :oreder_details do |t|
 
+      t.integer :item_id
+      t.integer :order_id
+      t.integer :amount, default: 0
+      t.integer :making_status, default: 0
+      t.integer :price
+
       t.timestamps
     end
   end
