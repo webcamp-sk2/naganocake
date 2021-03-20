04e228f84988ed5fd81198cd4ecb2dfd3cda9253
kayamayoki <k32142004@gmail.com>
2021-03-20 05:51:04 +0000

    レイアウト＋ルーティング一部修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,25 +9,21 @@ before_action :authenticate_public!
 
 #顧客の登録情報編集画面
   def edit
-    @customer = Customer.find(params[:id])
   end
 
 #顧客の登録情報更新
   def update
-    @customer = Customer.find(params[:id])
-    @customer.update(customer_params)
-    redirect_to customer_path(@customer.id)
+    current_public.update(public_params)
+    redirect_to public_customer_path(current_public)
   end
 
 #顧客の退会確認画面
   def unsubscribe
-    @customer = Customer.find_by(id: current_customer)
   end
 
 #顧客の退会処理(ステータスの更新)
   def withdraw
-    @customer = Customer.find_by(id: current_customer)
-    @customer.update(is_deleted: true)
+    current_public.update(is_deleted: false)
     reset_session
     redirect_to root_path
   end
@@ -35,8 +31,8 @@ before_action :authenticate_public!
 
 private
 
-  def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :address, :postal_code, :telephone_number)
+  def public_params
+    params.require(:public).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :address, :postal_code, :telephone_number)
   end
 
 end
